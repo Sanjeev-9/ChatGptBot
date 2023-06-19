515c26cbac11c876c1967fbcd23a23df6914534a
Sanjeev-9 <87819698+Sanjeev-9@users.noreply.github.com>
2023-06-19 16:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index_get():
     return render_template("base.html")
 
 
-@app.route("/predict",methods=["POST"])
+@app.route("https://sanjeev-9.github.io/ChatGptBot/predict",methods=["POST"])
 def predict():
     text = request.get_json().get("message")
     response = get_response(text)
